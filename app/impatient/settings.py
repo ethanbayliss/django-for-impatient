eabd1595abda98380d3c40b7e4a62810036c8d72
Ethan Bayliss <8937797+ethanbayliss@users.noreply.github.com>
2022-09-30 14:22:54 +0000

    finish db migration

--- app/impatient/settings.py
+++ app/impatient/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = "impatient.wsgi.application"
 # }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": "postgres",
         "USER" : "postgres",
         "PASSWORD" : "postgres",
