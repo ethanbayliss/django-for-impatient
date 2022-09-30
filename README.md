acae2d3b51c486195ba282bc6f46bf3b6e488ea3
Ethan Bayliss <8937797+ethanbayliss@users.noreply.github.com>
2022-09-30 13:57:46 +0000

    dev work

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 https://learning.oreilly.com/library/view/django-4-for/9781803245836/ 
+https://github.com/PacktPublishing/Django-4-for-the-Impatient
+ISBN-13: 978-1803245836
+
+
+
+## Development
+[Open the codespace](https://github.com/ethanbayliss/django-for-impatient/codespaces)
+```
+#create venv
+virtualenv venv
+
+#source venv
+source /workspaces/django-for-impatient/venv/bin/activate
+
+#install reqs
+pip install -r app/requirements.txt
+
+#run dev server
+python app/manage.py runserver
+```
--- app/movie/views.py
+++ app/movie/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+'''Movie app views'''
 
+from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    searchTerm = request.GET.get('searchMovie')
-    return render(request, 'home.html', {'searchTerm': searchTerm})
+from .models import Movie
 
-def about(request):
+def home(request):
+    '''Home page'''
+    search_term = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': search_term, 'movies': movies})
+
+def about():
+    '''About page'''
     return HttpResponse('<h1>about</h1')
 
 def signup(request):
+    '''Signup page. Takes in email and renders successful signup page for user'''
     email = request.GET.get('email')
     return render(request, 'signup.html', {'email': email})
